2bad46bb1e975e1cee2adab7cb02a8834fedfa2c
sergiospad <Lordgrey663@gmail.com>
2025-12-18 02:26:29 +0400

    Первичный бульон

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes :Routes = [
+  // {path: 'login', component:LoginComponent},
+  // {path: 'register', component: RegisterComponent},
+  // {path: 'main', component: IndexComponent, canActivate:[AuthGuardService]},
+  // {path: 'profile', component: ProfileComponent, canActivate:[AuthGuardService], children:[
+  //     {path: '', component: UserPostsComponent, canActivate: [AuthGuardService]},
+  //     {path: 'add', component: AddPostComponent, canActivate: [AuthGuardService]}
+  //   ]},
+  // {path: '', redirectTo: 'main', pathMatch: 'full'}
+];
+
+// export const authInterceptorProviders =[
+//   {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+// ];
+
